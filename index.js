33fa557b0544a613ec8cdd12ec5d7d38e9dbc1c1
Deepak Mardi <deepakmardi22@gmail.com>
2024-02-13 13:25:02 +0530

    Added custom Authentication

--- index.js
+++ index.js
@@ -1,113 +1,130 @@
 const express = require("express");
-const PORT = 3000;
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 
-const ADMINS = [];
-const USERS = [];
-const COURSES = [];
+let ADMINS = [];
+let USERS = [];
+let COURSES = [];
 
-// ADMIN ROUTES
-app.post("/admin/signup", (req, res) => {
-  //Admin signup logic
-  const admin = req.body;
-  ADMINS.push(admin);
-  res.json({ message: "Admin Signed Successfully" });
-});
-
-app.post("/admin/login", (req, res) => {
-  //Admin login logic
+const adminAuthentication = (req, res, next) => {
   const { username, password } = req.headers;
+
   const admin = ADMINS.find(
     (a) => a.username === username && a.password === password
   );
   if (admin) {
-    res.json({ message: "Logged in successfully" });
+    next();
   } else {
-    res.json({ message: "Admin authentication failed" });
+    res.status(403).json({ message: "Admin authentication failed" });
   }
-});
+};
 
-app.post("/admin/courses", (req, res) => {
-  // Create a course logic
-  const course = req.body;
-
-  course.id = Date.now(); // Using timestamp to use as course ID
-  COURSES.push(course);
-  res.json({ message: "Course Created Successfully", courseId: course.id });
-});
-
-app.post("/admin/courses/:courseId", (req, res) => {
-  // Edit a course logic
-  const courseId = parseInt(req.params.courseId);
-  const course = COURSES.find((c) => c.id === courseId);
-  if (course) {
-    Object.assign(course, req.body);
-    res.json({ message: "Course Updated Successfully" });
-  } else {
-    res.sendStatus(403).json({ message: "Course not found" });
-  }
-});
-
-app.get("/admin/courses", (req, res) => {
-  // Show all course
-  res.json({ courses: COURSES });
-});
-
-//USER ROUTES
-app.post("/user/signup", (req, res) => {
-  //User signup logic
-  const user = req.body;
-  USERS.push(user);
-  res.json({ message: "User Added Successfully" });
-});
-
-app.post("/user/login", (req, res) => {
-  //User login logic
+const userAuthentication = (req, res, next) => {
   const { username, password } = req.headers;
   const user = USERS.find(
     (u) => u.username === username && u.password === password
   );
   if (user) {
-    res.json({ message: "User logged in sucsessfuly" });
+    req.user = user; // Add user object to the request
+    next();
   } else {
-    res.json({ message: "User doesnot exist" });
+    res.status(403).json({ message: "User authentication failed" });
+  }
+};
+
+app.post("/admin/signup", (req, res) => {
+  const admin = req.body;
+  const existingAdmin = ADMINS.find((a) => a.username === admin.username);
+  if (existingAdmin) {
+    res.status(403).json({ message: "Admin already exists" });
+  } else {
+    ADMINS.push(admin);
+    res.json({ message: "Admin created successfully" });
   }
 });
 
-app.get("/user/courses", (req, res) => {
-  // Logic to see all courses
-  res.json(COURSES);
+app.post("/admin/login", adminAuthentication, (req, res) => {
+  res.json({ message: "Logged in successfully" });
 });
 
-app.post("/user/courses/:courseId", (req, res) => {
-  // Logic to buy a course
+app.post("/admin/courses", adminAuthentication, (req, res) => {
+  const course = req.body;
+
+  course.id = Date.now(); // use timestamp as course ID
+  COURSES.push(course);
+  res.json({ message: "Course created successfully", courseId: course.id });
+});
+
+app.put("/admin/courses/:courseId", adminAuthentication, (req, res) => {
   const courseId = parseInt(req.params.courseId);
   const course = COURSES.find((c) => c.id === courseId);
+  if (course) {
+    Object.assign(course, req.body);
+    res.json({ message: "Course updated successfully" });
+  } else {
+    res.status(404).json({ message: "Course not found" });
+  }
+});
+
+app.get("/admin/courses", adminAuthentication, (req, res) => {
+  res.json({ courses: COURSES });
+});
+
+app.post("/users/signup", (req, res) => {
+  // const user = {...req.body, purchasedCourses: []};
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    purchasedCourses: [],
+  };
+  USERS.push(user);
+  res.json({ message: "User created successfully" });
+});
+
+app.post("/users/login", userAuthentication, (req, res) => {
+  res.json({ message: "Logged in successfully" });
+});
+
+app.get("/users/courses", userAuthentication, (req, res) => {
+  // COURSES.filter(c => c.published)
+  let filteredCourses = [];
+  for (let i = 0; i < COURSES.length; i++) {
+    if (COURSES[i].published) {
+      filteredCourses.push(COURSES[i]);
+    }
+  }
+  res.json({ courses: filteredCourses });
+});
+
+app.post("/users/courses/:courseId", userAuthentication, (req, res) => {
+  const courseId = Number(req.params.courseId);
+  const course = COURSES.find((c) => c.id === courseId && c.published);
   if (course) {
     req.user.purchasedCourses.push(courseId);
     res.json({ message: "Course purchased successfully" });
   } else {
-    res.json({ message: "Course not found" });
+    res.status(404).json({ message: "Course not found or not available" });
   }
 });
 
-app.get("user/purchasedCourse", (req, res) => {
-  //Logic to see all purchased courses
+app.get("/users/purchasedCourses", userAuthentication, (req, res) => {
+  // const purchasedCourses = COURSES.filter(c => req.user.purchasedCourses.includes(c.id));
+  // We need to extract the complete course object from COURSES
+  // which have ids which are present in req.user.purchasedCourses
   var purchasedCourseIds = req.user.purchasedCourses;
+  [1, 4];
   var purchasedCourses = [];
-  // purchasedCoursesIds.forEach((id) => {
-  //   purchasedCourses.push(COURSES.find((c) => c.id === id));
-  // });    OR
   for (let i = 0; i < COURSES.length; i++) {
     if (purchasedCourseIds.indexOf(COURSES[i].id) !== -1) {
       purchasedCourses.push(COURSES[i]);
     }
   }
+
   res.json({ purchasedCourses });
 });
 
 app.listen(PORT, () => {
-  console.log(`Listining on port ${PORT}`);
+  console.log("Server is listening on port 3000");
 });
